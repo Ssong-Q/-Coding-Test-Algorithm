438326894781aaab4f02d06e4010192008c45f04
KyuKyoung Song (James Song) <122101706+Ssong-Q@users.noreply.github.com>
2023-09-24 22:26:23 +0900

    [unrated] Title: 조건 문자열, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.py
@@ -0,0 +1,14 @@
+def solution(ineq, eq, n, m):
+    answer = 0
+    if ineq == '>':
+        if eq == '=':
+            answer = int(n >= m)
+        else:
+            answer = int(n > m)
+    else:
+        if eq == '=':
+            answer = int(n <= m)
+        else:
+            answer = int(n < m)
+    return answer
+
