c828393c475a98f40f47779324277a38d61f46c0
KyuKyoung Song (James Song) <122101706+Ssong-Q@users.noreply.github.com>
2023-10-05 22:34:11 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.js
+++ 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.js
@@ -1,16 +1,16 @@
 function solution(num, total) {
   let answer = [];
   if (num % 2 === 1) {
-    const MIDDLE_VALUE = total / num;
-    const FIRST_VALUE = MIDDLE_VALUE - (num - 1) / 2;
+    const middleValue = total / num;
+    const firstValue = middleValue - (num - 1) / 2;
     for (let i = 0; i < num; i++) {
-      answer.push(FIRST_VALUE + i);
+      answer.push(firstValue + i);
     }
   } else {
-    const MIDDLE_VALUE = Math.floor(total / num);
-    const FIRST_VALUE = MIDDLE_VALUE - (num - 2) / 2;
+    const middleValue = Math.floor(total / num);
+    const firstValue = middleValue - (num - 2) / 2;
     for (let i = 0; i < num; i++) {
-      answer.push(FIRST_VALUE + i);
+      answer.push(firstValue + i);
     }
   }
 
