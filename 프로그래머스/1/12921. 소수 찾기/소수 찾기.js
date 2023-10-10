449b063e7cec4a7ee99c241dd16784f2c6cedeff
KyuKyoung Song (James Song) <122101706+Ssong-Q@users.noreply.github.com>
2023-10-11 00:03:57 +0900

    [level 1] Title: 소수 찾기, Time: 40.22 ms, Memory: 42.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -0,0 +1,20 @@
+function solution(n) {
+  let answer = 0;
+  let arr = new Array(n + 1).fill(true).fill(false, 0, 2);
+
+  for (let i = 2; i <= n; i++) {
+    if (arr[i] === false) {
+      continue;
+    }
+    for (let k = i * 2; k <= n; k += i) {
+      arr[k] = false;
+    }
+  }
+
+  for (let i = 0; i <= n; i++) {
+    if (arr[i] === true) {
+      answer++;
+    }
+  }
+  return answer;
+}
