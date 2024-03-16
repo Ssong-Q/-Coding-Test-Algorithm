d5bb17fbfcd7e1389781a59f9e046b93a9bb71a0
KyuKyoung Song (James Song) <skdmlrh12@gmail.com>
2024-03-16 13:17:26 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,11 +1,33 @@
+/**
+ * JadenCase 문자열 만들기 - 문자열이 들어오면 모든 단어의 첫 문자가 대문자이고, 그 외의 알파벳은 소문자인 문자열로 만드는 것
+ * @param {string} s 문자열
+ * @returns JadenCase 문자열
+ */
+
 function solution(s) {
-  const arr = s.split(" ");
-  for (let i = 0; i < arr.length; i++) {
-    arr.splice(
-      i,
-      1,
-      arr[i].slice(0, 1).toUpperCase() + arr[i].slice(1).toLowerCase()
-    );
-  }
-  return arr.join(" ");
+  let answer = "",
+    temp = [];
+
+  s.split(" ").forEach((val) => {
+    if (val === "") {
+      temp.push("");
+      return;
+    }
+
+    if (
+      !(val[0].charCodeAt() >= 65 && val[0].charCodeAt() <= 90) &&
+      !(val[0].charCodeAt() >= 97 && val[0].charCodeAt() <= 122)
+    ) {
+      val = val[0] + val.slice(1).toLowerCase();
+      temp.push(val);
+      return;
+    }
+
+    val = val[0].toUpperCase() + val.slice(1).toLowerCase();
+    temp.push(val);
+  });
+
+  answer = temp.join(" ");
+
+  return answer;
 }
--- 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
@@ -1,6 +1,6 @@
 # [level 2] JadenCase 문자열 만들기 - 12951 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951#) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 0일 21:12:38
+2024년 03월 16일 13:17:23
 
 ### 문제 설명
 
