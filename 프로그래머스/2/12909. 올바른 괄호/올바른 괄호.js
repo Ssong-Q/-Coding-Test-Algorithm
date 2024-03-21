270d96068cce8d67053ad9515aac9079a273ce91
KyuKyoung Song (James Song) <skdmlrh12@gmail.com>
2024-03-21 15:21:34 +0900

    [level 2] Title: 올바른 괄호, Time: 5.73 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,12 +1,13 @@
 function solution(s) {
-  let cnt = 0;
+  let answer = true,
+    stack = [];
+
   for (let i = 0; i < s.length; i++) {
-    if (s[i] === "(") {
-      cnt += 1;
-    } else {
-      cnt -= 1;
-    }
-    if (cnt < 0) return false;
+    if (stack[stack.length - 1] === "(" && s[i] === ")") stack.pop();
+    else stack.push(s[i]);
   }
-  return cnt === 0;
+
+  answer = stack.length ? false : true;
+
+  return answer;
 }
