e3277d3d0806817e921c0ff28812bf81df61fb42
KyuKyoung Song (James Song) <skdmlrh12@gmail.com>
2024-03-29 23:38:59 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
@@ -0,0 +1,10 @@
+function solution(my_string, overwrite_string, s) {
+  let answer = "",
+    arr = my_string.split("");
+
+  arr.splice(s, overwrite_string.length, overwrite_string);
+
+  answer = arr.join("");
+
+  return answer;
+}
