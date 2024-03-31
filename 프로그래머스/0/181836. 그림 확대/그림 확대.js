f4edc86c4bf844524625f8630f31219fd2b27956
KyuKyoung Song (James Song) <skdmlrh12@gmail.com>
2024-04-01 00:21:12 +0900

    [level 0] Title: 그림 확대, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.js
@@ -0,0 +1,13 @@
+function solution(picture, k) {
+  let answer = [];
+
+  for (let x of picture) {
+    let temp = "";
+    for (let y of x) {
+      for (let i = 0; i < k; i++) temp += y;
+    }
+    for (let i = 0; i < k; i++) answer.push(temp);
+  }
+
+  return answer;
+}
