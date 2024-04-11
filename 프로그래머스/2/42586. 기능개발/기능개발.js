93defe881c75b3ddb9a1a08090ee3c055cac82ce
KyuKyoung Song (James Song) <skdmlrh12@gmail.com>
2024-04-11 23:17:34 +0900

    [level 2] Title: 기능개발, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,32 +1,23 @@
 function solution(progresses, speeds) {
-  let answer = [];
-  let stack = [];
-  let count = 0;
-  let stackDay = 0;
-  for (let i = 0; i < progresses.length; i++) {
-    const day = Math.ceil((100 - progresses[i]) / speeds[i]);
-    const nextDay = Math.ceil((100 - progresses[i + 1]) / speeds[i + 1]);
-    if (!stack.length) {
-      stack.push(progresses[i]);
-      stackDay = day;
-      count = 1;
-      if (nextDay > stackDay && i !== progresses.length - 1) {
-        answer.push(count);
-        stack.pop();
-        count = 0;
-      } else if (i === progresses.length - 1) {
-        answer.push(count);
-      }
+  let answer = [],
+    temp = 1,
+    tempArr = [];
+
+  const leftDays = progresses.map((el, idx) => {
+    return Math.ceil((100 - el) / speeds[idx]);
+  });
+
+  tempArr.push(leftDays[0]);
+  for (let i = 1; i < leftDays.length; i++) {
+    if (tempArr[tempArr.length - 1] < leftDays[i]) {
+      tempArr.push(leftDays[i]);
+      answer.push(temp);
+      temp = 1;
     } else {
-      count += 1;
-      if (nextDay > stackDay && i !== progresses.length - 1) {
-        answer.push(count);
-        stack.pop();
-        count = 0;
-      } else if (i === progresses.length - 1) {
-        answer.push(count);
-      }
+      temp++;
     }
   }
+  answer.push(temp);
+
   return answer;
 }
