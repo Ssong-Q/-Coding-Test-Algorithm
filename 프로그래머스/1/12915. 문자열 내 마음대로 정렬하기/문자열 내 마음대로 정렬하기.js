0b630a98c2d52ab628eaf900c59a517b8d541afc
KyuKyoung Song (James Song) <skdmlrh12@gmail.com>
2024-04-13 14:02:53 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 350.86 ms, Memory: 35.9 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -1,15 +1,12 @@
 function solution(strings, n) {
-  return strings.sort((a, b) => {
-    if (a[n] > b[n]) {
-      return 1;
-    } else if (a[n] < b[n]) {
-      return -1;
-    } else if (a[n] === b[n]) {
-      if (a > b) {
-        return 1;
-      } else if (a < b) {
-        return -1;
-      }
-    }
+  let answer = [];
+
+  strings.sort((a, b) => {
+    if (a[n] === b[n]) return a.localeCompare(b);
+    else return a[n].localeCompare(b[n]);
   });
+
+  answer = strings;
+
+  return answer;
 }
