add48bfbbab6d962f863b4a7c70d617e23b997f7
KyuKyoung Song (James Song) <skdmlrh12@gmail.com>
2024-04-17 00:47:21 +0900

    [level 2] Title: 예상 대진표, Time: 0.19 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -0,0 +1,17 @@
+function solution(n, a, b) {
+  let answer = 1;
+
+  while (true) {
+    if (
+      (a < b && a % 2 === 1 && a + 1 === b) ||
+      (a > b && b % 2 === 1 && b + 1 === a)
+    )
+      break;
+
+    a = Math.ceil(a / 2);
+    b = Math.ceil(b / 2);
+    answer++;
+  }
+
+  return answer;
+}
