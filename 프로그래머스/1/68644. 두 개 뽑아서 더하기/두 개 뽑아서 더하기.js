795258ed8f36fa5cd8ef97d2e313cd3090e2d3e5
KyuKyoung Song (James Song) <skdmlrh12@gmail.com>
2024-04-17 15:33:03 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.84 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -1,11 +1,15 @@
 function solution(numbers) {
-  let answer = [];
+  let answer = [],
+    result = [];
+
   for (let i = 0; i < numbers.length - 1; i++) {
     for (let j = i + 1; j < numbers.length; j++) {
-      answer.push(numbers[i] + numbers[j]);
+      result.push(numbers[i] + numbers[j]);
     }
   }
+
+  answer = [...new Set(result)];
   answer.sort((a, b) => a - b);
-  answer = [...new Set(answer)];
+
   return answer;
 }
