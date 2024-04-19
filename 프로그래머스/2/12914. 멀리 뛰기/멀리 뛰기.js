89ca330c98b091aa6ffd810720c9dfd71e32b9c1
KyuKyoung Song (James Song) <skdmlrh12@gmail.com>
2024-04-19 20:13:36 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,9 +1,19 @@
 function solution(n) {
   if (n === 1) return 1;
   if (n === 2) return 2;
-  let arr = [1, 2];
-  for (let i = 1; i < n - 1; i++) {
-    arr.push((arr[i] + arr[i - 1]) % 1234567);
+
+  let answer = 0,
+    a = 1,
+    b = 2,
+    temp = 0;
+
+  for (let i = 3; i <= n; i++) {
+    temp = (a % 1234567) + (b % 1234567);
+    a = b;
+    b = temp;
   }
-  return arr[n - 1];
+
+  answer = b % 1234567;
+
+  return answer;
 }
