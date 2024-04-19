fc6ef76c051a345e5412b3341b77d37fbf1869cc
KyuKyoung Song (James Song) <skdmlrh12@gmail.com>
2024-04-19 15:45:09 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -0,0 +1,14 @@
+function solution(chicken) {
+  let answer = 0,
+    coupon = chicken;
+
+  while (coupon >= 10) {
+    const service = Math.floor(coupon / 10);
+    answer += service;
+
+    const leftCoupon = coupon - service * 10;
+    coupon = service + leftCoupon;
+  }
+
+  return answer;
+}
