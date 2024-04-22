615bf0990083f28cd5e0aeccc8c01633aff6c271
KyuKyoung Song (James Song) <skdmlrh12@gmail.com>
2024-04-22 09:47:47 +0900

    [level 1] Title: 체육복, Time: 0.12 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -0,0 +1,47 @@
+function solution(n, lost, reserve) {
+  let answer = n - lost.length,
+    temp = [];
+
+  reserve.sort((a, b) => a - b);
+
+  // 같은 것 제거
+  for (let idx = 0; idx < reserve.length; idx++) {
+    if (!lost.length) break;
+
+    if (lost.includes(reserve[idx])) {
+      // 같은 값일 때
+      const targetIdx = lost.findIndex((val) => val === reserve[idx]);
+      lost.splice(targetIdx, 1);
+      temp.push(reserve[idx]);
+      answer++;
+    }
+  }
+
+  // 같은 것 제거한 것을 reserve에서 제거
+  for (let idx = 0; idx < temp.length; idx++) {
+    const targetIdx = reserve.findIndex((val) => val === temp[idx]);
+    reserve.splice(targetIdx, 1);
+  }
+
+  // 나머지 reserve에서 아래, 위 숫자 처리
+  for (let idx = 0; idx < reserve.length; idx++) {
+    if (!lost.length) break;
+
+    if (lost.includes(reserve[idx] - 1)) {
+      // 작은 값일 때 (작은 값 우선)
+      const targetIdx = lost.findIndex((val) => val === reserve[idx] - 1);
+      lost.splice(targetIdx, 1);
+      answer++;
+      continue;
+    }
+
+    if (lost.includes(reserve[idx] + 1)) {
+      // 큰 값일 때
+      const targetIdx = lost.findIndex((val) => val === reserve[idx] + 1);
+      lost.splice(targetIdx, 1);
+      answer++;
+    }
+  }
+
+  return answer;
+}
