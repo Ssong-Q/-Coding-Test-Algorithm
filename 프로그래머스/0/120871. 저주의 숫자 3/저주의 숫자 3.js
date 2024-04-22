7c6910c1604626b8dbef2ce02ce648fc8e0f98d0
KyuKyoung Song (James Song) <skdmlrh12@gmail.com>
2024-04-22 12:10:30 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.14 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -0,0 +1,19 @@
+function solution(n) {
+  let answer = 0,
+    result = [],
+    i = 1;
+
+  while (result.length <= 100) {
+    if (i % 3 === 0 || i.toString().includes("3")) {
+      i++;
+      continue;
+    }
+
+    result.push(i);
+    i++;
+  }
+
+  answer = result[n - 1];
+
+  return answer;
+}
